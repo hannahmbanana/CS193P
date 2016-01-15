4852be19a8deaa9d4182b49b43f0d40cb7077080
Hannah Troisi <htroisi@gmail.com>
2016-01-14 22:46:03 -0800

    Updated DropIt (my no storyboard version) after lecture 9.

---
+++ Assignment_4_GraphicalSet_iOS9/DropIt/DropIt/AnchorBarView.m
@@ -0,0 +1,33 @@
+//
+//  AnchorBarView.m
+//  DropIt
+//
+//  Created by Hannah Troisi on 1/14/16.
+//  Copyright © 2016 Hannah Troisi. All rights reserved.
+//
+
+#import "AnchorBarView.h"
+
+@implementation AnchorBarView
+
+
+# pragma mark - Properties
+
+- (void)setPath:(UIBezierPath *)path
+{
+  _path = path;
+  
+  // update view every time path is set
+  [self setNeedsDisplay];
+}
+
+
+# pragma mark - Lifecycle
+
+- (void)drawRect:(CGRect)rect
+{
+  [self.path stroke];
+}
+
+
+@end
